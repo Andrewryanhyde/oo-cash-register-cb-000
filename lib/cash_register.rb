2b8100457389dfef4931f6275fe9ddf88d30945b
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-22 12:52:47 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -26,7 +26,7 @@ class CashRegister
   def apply_discount
     perc_after_disc = 100 - @discount
     @new_total = @total / 100 * perc_after_disc
-    puts "After the discount, the total comes to #{@total}."
+    puts "After the discount, the total comes to #{@new_total}."
   end
 
 
