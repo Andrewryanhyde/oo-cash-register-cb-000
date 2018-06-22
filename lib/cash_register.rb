4d15f084c5799be050eb5f5e60211dd93b2fd47a
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-22 12:54:02 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -24,9 +24,12 @@ class CashRegister
   end
 
   def apply_discount
-    perc_after_disc = 100 - @discount
-    @new_total = @total / 100 * perc_after_disc
-    puts "After the discount, the total comes to #{@new_total}."
+    if @discount == true
+      perc_after_disc = 100 - @discount
+      @new_total = @total / 100 * perc_after_disc
+      puts "After the discount, the total comes to #{@new_total}."
+    else 
+      puts "There is no discount to apply."
   end
 
 
