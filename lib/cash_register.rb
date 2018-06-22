202be3825e0e9333c22c5c4f37c4de17e4791ccd
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-22 13:23:04 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,16 +14,17 @@ class CashRegister
   end
 
   def add_item(item, price, quantity = 1)
+    @price = price
     @total += price * quantity
-      if quantity > 1
-        counter = 0
-        while counter < quantity
-          @items << item
-          counter += 1
-        end
-      else
+    if quantity > 1
+      counter = 0
+      while counter < quantity
         @items << item
+        counter += 1
       end
+    else
+      @items << item
+    end
   end
 
   def apply_discount
